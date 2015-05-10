477a4b3a6149df7b156fee44ca9a8550e78ce230
Mark Azemoon <mmazemoon@gmail.com>
2015-05-09 18:43:06 -0700

    Handle CREATE and INDEX requests from client

--- bin/cat_server.rb
+++ bin/cat_server.rb
@@ -1,25 +1,29 @@
 require 'socket'
 require 'thread'
+require 'json'
+
+$cats = []
 
 server = TCPServer.new (3000)
 
 def handle_request(socket)
-  Thread.new do                     # master thread spawning worker threads.
-    socket.puts("THANKS FOR CONNECTING")
-    socket.puts("What is your name, human?")
-    name = socket.gets.chomp
-    socket.puts("Goodbye #{name}")
+  Thread.new do
+    cmd = socket.gets.chomp
+    case cmd
+    when "INDEX"
+      socket.puts $cats.to_json
+    when "CREATE"
+      name = socket.gets.chomp
+      $cats << { name: name }
+    end
+
     socket.close
   end
-  puts "Spawned worker thread"    # master thread moves on
-end                               # while worker thread waits for input.
+  puts "Spawned worker thread"
+end
 
 
 while true
   socket = server.accept
   handle_request(socket)
 end
-
-
-# can't accept another connection because ruby is too busy
-# waiting for this socket to send data
