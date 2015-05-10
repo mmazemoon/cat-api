fa2d8cb840245aae3f81dc02556ba94fafee3daa
Mark Azemoon <mmazemoon@gmail.com>
2015-05-09 18:02:02 -0700

    Add method to include master and worker threads to handle multiple client requests

--- bin/cat_server.rb
+++ bin/cat_server.rb
@@ -3,13 +3,21 @@ require 'thread'
 
 server = TCPServer.new (3000)
 
+def handle_request(socket)
+  Thread.new do                     # master thread spawning worker threads.
+    socket.puts("THANKS FOR CONNECTING")
+    socket.puts("What is your name, human?")
+    name = socket.gets.chomp
+    socket.puts("Goodbye #{name}")
+    socket.close
+  end
+  puts "Spawned worker thread"    # master thread moves on
+end                               # while worker thread waits for input.
+
+
 while true
   socket = server.accept
-  socket.puts("THANKS FOR CONNECTING")
-  socket.puts("What is your name, human?")
-  name = socket.gets.chomp
-  socket.puts("Goodbye #{name}")
-  socket.close
+  handle_request(socket)
 end
 
 
