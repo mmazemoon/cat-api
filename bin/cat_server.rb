533323f8ffa1d295a6922f120cac7b73cf2f7552
Mark Azemoon <mmazemoon@gmail.com>
2015-05-10 19:27:28 -0700

    Add DESTROY, UPDATE, and SHOW behavior

--- bin/cat_server.rb
+++ bin/cat_server.rb
@@ -2,9 +2,20 @@ require 'socket'
 require 'thread'
 require 'json'
 
+# TCP to transfer data back and forth from client and server.
+# Bespoke protocol (format of the messages between c&s)
+# Replace with HTTP. (Hypertext Transfer Protocol)
+# HTML (Hypertext Markup Language)
+
+
+Thread.abort_on_exception = true
+
+$id = 0
 $cats = []
 
 server = TCPServer.new (3000)
+
+# CRUD: CREATE/READ/UPDATE/DELETE servers
 
 def handle_request(socket)
   Thread.new do
@@ -12,16 +23,43 @@ def handle_request(socket)
     case cmd
     when "INDEX"
       socket.puts $cats.to_json
-    when "CREATE"
-      name = socket.gets.chomp
-      $cats << { name: name }
-    end
+    when "SHOW"
+      # GET /cats/:id
+      # GET /cats/123
+      # GET /dogs/456
 
+      cat_id = Integer(socket.gets.chomp)
+      cat = $cats.find{ |cat| cat[:id] == cat_id }
+      socket.puts cat.to_json
+    when "UPDATE"
+      # PATCH /cats/123
+      # PATCH /cats/456
+      cat_id = Integer(socket.gets.chomp)
+      cat = $cats.find{ |cat| cat[:id] == cat_id }
+
+      new_name = socket.gets.chomp
+      cat[:name] = new_name
+    when "CREATE"
+      # POST /cats
+      # POST /dogs
+
+
+      name = socket.gets.chomp
+      cat_id = $id
+      $id += 1
+
+      $cats << { id: cat_id, name: name }
+    when "DESTROY"
+      # DELETE /cats/123
+      # DELETE /dogs/456
+
+      cat_id = Integer(socket.gets.chomp)
+      $cats.reject! { |cat| cat[:id] == cat_id }
+    end
     socket.close
   end
   puts "Spawned worker thread"
 end
-
 
 while true
   socket = server.accept
