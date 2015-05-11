2ab993da6050e86b0bd6292ecaff16c97c2ea243
Mark Azemoon <mmazemoon@gmail.com>
2015-05-10 20:22:39 -0700

    Convert SHOW, INDEX, DESTROY to GET and DELETE

--- bin/cat_server.rb
+++ bin/cat_server.rb
@@ -7,58 +7,68 @@ require 'json'
 # Replace with HTTP. (Hypertext Transfer Protocol)
 # HTML (Hypertext Markup Language)
 
-
 Thread.abort_on_exception = true
 
-$id = 0
-$cats = []
+$id = 2
+$cats = [
+  {id: 1, name: "Wonton"},
+  {id: 2, name: "Curie"}
+  ]
 
 server = TCPServer.new (3000)
-
 # CRUD: CREATE/READ/UPDATE/DELETE servers
 
 def handle_request(socket)
-  Thread.new do
-    cmd = socket.gets.chomp
-    case cmd
-    when "INDEX"
-      socket.puts $cats.to_json
-    when "SHOW"
-      # GET /cats/:id
-      # GET /cats/123
-      # GET /dogs/456
 
-      cat_id = Integer(socket.gets.chomp)
+  Thread.new do
+    # method path protocol_version
+    line1 = socket.gets.chomp
+
+    re = /^([^ ]+) ([^ ]+) HTTP\/1.1$/
+    match_data = re.match(line1)
+    verb = match_data[1]
+    path = match_data[2]
+
+    cat_regex = /\/cats\/(\d+)/
+
+    if [verb, path] == ["GET", "/cats"] # INDEX
+      # GET /cats
+      socket.puts $cats.to_json
+    elsif verb == "GET" && cat_regex =~ path # SHOW
+      # GET /cats/123
+      match_data2 = cat_regex.match(path)
+      cat_id = Integer(match_data2[1])
+
       cat = $cats.find{ |cat| cat[:id] == cat_id }
       socket.puts cat.to_json
-    when "UPDATE"
-      # PATCH /cats/123
-      # PATCH /cats/456
-      cat_id = Integer(socket.gets.chomp)
-      cat = $cats.find{ |cat| cat[:id] == cat_id }
-
-      new_name = socket.gets.chomp
-      cat[:name] = new_name
-    when "CREATE"
-      # POST /cats
-      # POST /dogs
-
-
-      name = socket.gets.chomp
-      cat_id = $id
-      $id += 1
-
-      $cats << { id: cat_id, name: name }
-    when "DESTROY"
+    elsif verb == "DELETE" && cat_regex =~ path
       # DELETE /cats/123
-      # DELETE /dogs/456
-
-      cat_id = Integer(socket.gets.chomp)
+      match_data2 = cat_regex.match(path)
+      cat_id = Integer(match_data2[1])
       $cats.reject! { |cat| cat[:id] == cat_id }
+
+      socket.puts true.to_json
     end
-    socket.close
-  end
-  puts "Spawned worker thread"
+
+      # when "UPDATE"
+      #   # PATCH /cats/123
+      #   # PATCH /cats/456
+      #   cat_id = Integer(socket.gets.chomp)
+      #   cat = $cats.find{ |cat| cat[:id] == cat_id }
+      #
+      #   new_name = socket.gets.chomp
+      #   cat[:name] = new_name
+      # when "CREATE"
+      #   # POST /cats
+      #   # POST /dogs
+      #   name = socket.gets.chomp
+      #   cat_id = $id
+      #   $id += 1
+      #   $cats << { id: cat_id, name: name }
+
+      socket.close
+    end
+      puts "Spawned worker thread"
 end
 
 while true
